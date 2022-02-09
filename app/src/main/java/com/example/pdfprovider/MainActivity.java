eade3759e37ef8a2141bf14211c2bca8994bee15
Emre Akcan <emrecanakcan1@gmail.com>
2022-02-09 10:16:14 +0000

    initial commit

---
+++ app/src/main/java/com/example/pdfprovider/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.pdfprovider;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ProviderInfo;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        for (PackageInfo pack : getPackageManager().getInstalledPackages(PackageManager.GET_PROVIDERS)) {
+            ProviderInfo[] providers = pack.providers;
+            if (providers != null) {
+                for (ProviderInfo provider : providers) {
+                    Log.d("Example", "provider: " + provider.authority);
+                }
+            }
+        }
+    }
+}
